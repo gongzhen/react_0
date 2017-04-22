d499c60f400acada530634ddbc82471bb9febd69
gongzhen <gongzhengz@gmail.com>
2017-04-21 18:05:12 -0700

    demo03 fixed unique key issue.

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const App = () => {
         <div>
         {
           arr.map((name,index) => {
-            return <div>{index}:{name}</div>
+            return <div key={index}>{index}:{name}</div>
           })
         }
         </div>
